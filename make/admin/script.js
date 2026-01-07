50760b03e1b3e108042ce695bde26cdefb98702f
Víctor Manuel Nuñez Salas <122834431+VictorMNSalas@users.noreply.github.com>
2026-01-07 13:28:00 -0600

    format url api

--- make/admin/script.js
+++ make/admin/script.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:3000/api/v1";
-// const API_URL = "https://make-gold.vercel.app/api/v1";
+// const API_URL = "http://localhost:3000/api/v1";
+const API_URL = "https://make-gold.vercel.app/api/v1";
 
 //
 const months = [
